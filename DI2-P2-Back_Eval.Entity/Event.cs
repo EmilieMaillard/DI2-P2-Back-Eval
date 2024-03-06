eb1c5ada8bfa919aae042c9c988df104b61d013d
EmilieM <emilie.maillard@diiage.org>
2024-03-06 13:51:55 +0100

    Add event model - title, description, date, location

---
+++ DI2-P2-Back_Eval.Entity/Event.cs
@@ -0,0 +1,11 @@
+namespace DI2_P2_Back_Eval.Entity;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    
+}
